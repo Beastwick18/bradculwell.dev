af6b32421c69c2f12006c63dddf3fe55b545c531
Brad Culwell <bradculwell@pm.me>
2024-02-02 20:00:02 -0600

    fit screen

--- components/aboutme.tsx
+++ components/aboutme.tsx
@@ -1,22 +1,21 @@
 import Lnk from "@/components/lnk";
-import Repo from "./repo";
 import Repos from "./repos";
 
 const AboutMe = () => {
   return (
-    <div id="about-me" className="w-lvw lg:h-lvh overflow-hidden flex flex-col">
+    <div id="about-me" className="w-lvw lg:h-lvh flex flex-col relative z-10">
       <div className="absolute w-lvw lg:h-lvh flex">
         <div className="w-1/3 h-lvh max-lg:h-full max-lg:w-0">
           <div style={{ backgroundImage: "url(tty.gif)" }} className="absolute w-full h-full max-lg:hidden bg-center bg-cover -z-10"></div>
           <div className="lg:bg-gradient-to-l lg:from-neutral-900 lg:to-transparent lg:from-65% w-full h-full max-lg:bg-neutral-900 lg:w-lvw lg:h-lvh absolute -z-10"></div>
         </div>
         <div className="lg:h-lvh w-2/3 max-lg:w-full bg-transparent flex flex-col items-center">
-          <div className="w-full lg:h-lvh max-lg:p-8 pt-12 flex flex-col gap-8">
-            <div className="lg:px-[8.3333lvw] w-fit h-fit">
+          <div className="w-full lg:h-lvh max-lg:p-8 pt-6 flex flex-col gap-6">
+            <div className="lg:mx-[8.3333lvw] w-fit h-fit">
               <h1 className="text-4xl">About Me</h1>
               <span className="bg-red-500 w-full h-1 block"></span>
             </div>
-            <div className="lg:px-[8.3333lvw] lg:overflow-scroll flex flex-col gap-8 text-2xl leading-9 lg:text-justify">
+            <div className="lg:mx-[8.3333lvw] lg:overflow-scroll flex flex-col gap-6 text-2xl leading-9 lg:text-justify">
               <p>
                 I&apos;m Brad Culwell, a dedicated student currently enrolled at The University of Texas at Arlington.
                 I&apos;m a Computer Science major who primarily works with languages like C, C++, Python, Rust, and Go, complemented by expertise in front-end technologies such as HTML, CSS, and JavaScript.
@@ -28,7 +27,7 @@ const AboutMe = () => {
                 Connect with me on <Lnk href="https://www.linkedin.com/in/steven-culwell-5b92b3239/">LinkedIn</Lnk> or explore my projects on <Lnk href="https://beastwick18.github.io">GitHub</Lnk>.
               </p>
             </div>
-            <div className="lg:px-[8.3333lvw] w-fit h-fit">
+            <div className="lg:mx-[8.3333lvw] w-fit h-fit">
               <h1 id="about-me" className="text-4xl">Projects</h1>
               <span className="bg-lime-500 w-full h-1 block"></span>
             </div>
--- components/mainpage.tsx
+++ components/mainpage.tsx
@@ -1,42 +1,38 @@
 import Image from "next/image"
 import Link from "next/link"
-import { FaEnvelope, FaGithub, FaLinkedin } from "react-icons/fa"
-import { GoChevronDown } from "react-icons/go"
+import { SiGithub, SiLinkedin, SiProtonmail } from "react-icons/si"
 
 const MainPage = () => {
   return (
     <div className="w-lvw h-lvh">
       <div className="fixed w-lvw h-lvh top-0 left-0">
         <div className="fixed top-0 left-0 w-lvw h-lvh">
-          <div style={{ backgroundImage: "url(minecraft.png)" }} className="w-lvw h-lvh bg-center bg-cover opacity-75"></div>
+          <div style={{ backgroundImage: "url(minecraft.png)" }} className="w-lvw h-lvh bg-center bg-cover"></div>
         </div>
         <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex md:flex-row text-center md:text-left flex-col gap-6 w-full items-center justify-center">
           <Link href="https://github.com/Beastwick18" className="rounded-full overflow-hidden w-fit h-full border-white/20 border">
             <Image alt="Profile Picture" src="https://github.com/beastwick18.png?size=200" width={200} height={200} />
           </Link>
           <div className="w-fit gap-2 flex flex-col font-light text-shadow">
-            <p className="md:text-7xl text-5xl">Brad Culwell</p>
+            <Link href="#about-me" className="md:text-7xl text-5xl">Brad Culwell</Link>
             <div className="md:text-3xl text-xl">
               <p>Computer Science Major</p>
               <p>University of Texas at Arlington</p>
             </div>
             <div className="flex flex-row gap-2 md:justify-start justify-center">
               <Link href="https://github.com/Beastwick18">
-                <FaGithub size={40} />
+                <SiGithub size={40} />
               </Link>
               <Link href="mailto:bradculwell@pm.me">
-                <FaEnvelope size={40} />
+                <SiProtonmail size={40} />
               </Link>
               <Link href="https://www.linkedin.com/in/steven-culwell-5b92b3239/">
-                <FaLinkedin size={40} />
+                <SiLinkedin size={40} />
               </Link>
             </div>
           </div>
         </div>
       </div>
-      <Link href="#about-me" className="fixed bottom-0 left-1/2 -translate-x-1/2">
-        <GoChevronDown size={60} className="animate-bounce" />
-      </Link>
     </div>
   )
 }
