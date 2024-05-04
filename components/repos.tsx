81a59763f6c70a5f69eaa215063c70b8a574071f
Brad Culwell <bradculwell@pm.me>
2024-05-03 22:37:36 -0500

    remove downloads

--- components/repos.tsx
+++ components/repos.tsx
@@ -17,7 +17,7 @@ const Repos = () => {
   }, [isVisible])
   return (
     <div ref={ref} className="lg:w-[66.6lvw] lg:px-[7.5lvw] flex-col w-full lg:overflow-scroll grid grid-cols-1 2xl:grid-cols-2 gap-4 text-2xl leading-9 text-neutral-400 items-center min-h-[200px] pb-4">
-      <Repo url="Beastwick18/nyaa" showDownloads={true} className={twMerge("animate-delay-0", animate)} />
+      <Repo url="Beastwick18/nyaa" className={twMerge("animate-delay-0", animate)} />
       <Repo url="Beastwick18/genshin-real-time-notes" className={twMerge("animate-delay-[.05s]", animate)} />
       <Repo url="Beastwick18/gltest" className={twMerge("animate-delay-[.1s]", animate)} />
       <Repo url="Beastwick18/kitty-background-manager" className={twMerge("animate-delay-[.15s]", animate)} />
